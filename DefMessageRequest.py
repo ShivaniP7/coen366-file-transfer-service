863c6108f39aff1863109fe99f43d377bc7ce011
ShivaniP7 <65041612+ShivaniP7@users.noreply.github.com>
2022-12-08 17:44:42 -0500

    Update DefMessageRequest.py

--- DefMessageRequest.py
+++ DefMessageRequest.py
@@ -1,35 +1,27 @@
 from DefToBinary import *
 
-def MessageRequest(userInput):
+def MessageResponse(messageRequest, validity, size):
     
     opcode = "000"
-    userInSplit=userInput.split()
-
-    if userInSplit[0] == 'put':
+    #MessageResponse=[]
+    messageRequestSplit=messageRequest.split()
+    if validity==0:
         opcode="000"
-    elif userInSplit[0] == 'get':
-        opcode= "001" 
-    elif userInSplit[0] == 'change':
-        opcode= "010" 
-    elif userInSplit[0] == 'help':
-        opcode= "011" 
-    elif userInSplit[0] == 'bye':
-        print ("end connection")
-    
-
-    if len(userInSplit)==2:    
-        messageRequest = [opcode, len(userInSplit[1]), userInSplit[1]]
-        binMessageRequest=[opcode, bin(len(userInSplit[1]))[2:], ' '.join (str(n) for n in toBinary(userInSplit[1]))]
-    
-    elif len(userInSplit)==1:
-        messageRequest = [opcode]
-        binMessageRequest = [opcode]
-    
-    elif len(userInSplit)==3:
-        messageRequest = [opcode, len(userInSplit[1]), userInSplit[1], len(userInSplit[2]), userInSplit[2]]
-        binMessageRequest=[opcode, bin(len(userInSplit[1]))[2:], ' '.join (str(n) for n in toBinary(userInSplit[1])), bin(len(userInSplit[2]))[2:], ' '.join (str(n) for n in toBinary(userInSplit[2]))]
-    
-
-        finalMessageRequest = ' '.join(str(n)for n in binMessageRequest)
-
-    return ' '.join(binMessageRequest)
+        MessageResponse = [opcode]
+    elif validity==1:
+        opcode="001"
+        MessageResponse = [messageRequest, str(bin(size)[2:])]
+    elif validity==2:
+        opcode="010"
+        MessageResponse = [opcode]
+    elif validity==3:
+        opcode="011"
+        MessageResponse = [opcode]
+    elif validity==4:
+        opcode="101"
+        MessageResponse = [opcode]
+    elif validity==5:
+        opcode="110"
+        MessageResponse = [opcode]
+        
+    return ' '.join(MessageResponse)
