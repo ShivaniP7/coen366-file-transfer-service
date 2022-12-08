20eb9e9cb52639e5900228595a67afb0430bc561
ShivaniP7 <65041612+ShivaniP7@users.noreply.github.com>
2022-12-08 17:45:44 -0500

    Update client.py

--- client.py
+++ client.py
@@ -3,32 +3,35 @@ from DefMessageRequest import *
 from DefToText import *
 from DefMessageResponse import *
 
-#userServerIP=str(input('input your server IP address: '))
-#userServerPort=int(input('input your server port: '))
-
+#!!! needs to be user input !!!
+#!!! userServerIP = input(str("Please enter the host address of the sender:"))
 userServerIP='192.168.0.101'
-userServerPort=49664
-
-userIn=input('input your instruction: ')
-userInWord=userIn.split()
+userServerPort=18000
 
 clientSocket=socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((userServerIP, userServerPort))
 
-userMessageRequest = MessageRequest(userIn).split()
+userIn=input('input your instruction: ')
+userInWord=userIn.split()
+#uIn=clientSocket.send(userIn.encode())
 
+userMessageRequest = MessageRequest(userIn).split()
+print(userMessageRequest)
 req=clientSocket.send(MessageRequest(userIn).encode())
 
 if userMessageRequest[0] == "000":
     with open(userInWord[1], 'rb') as file:
         data=file.read()
     ul=clientSocket.send(data)
+    print ("data sent to server")
     
 elif userMessageRequest[0] == "001":
     dl=clientSocket.recv(4096)
+    print ("data recieved from server")
     
 elif userMessageRequest[0] == "010":
     dl=clientSocket.recv(4096)
+    print ("file name changed")
 
 elif userMessageRequest[0] == "011":
     print("put change get help bye")
@@ -37,7 +40,6 @@ elif len(str(userMessageRequest[0])) != 3:
     print ("invalid opcode")
 
 resp=clientSocket.recv(4096)
+print("Response: " + resp.decode())
 
 clientSocket.close()
-
-
